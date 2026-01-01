7a4efae6cb630009000ac94158f4757fe6479d69
2025 <axmedovabdullatif>
2026-01-01 21:11:52 +0500

    1-dars: print(), comments va escape characters tushuntirildi

---
+++ Python asoslari/lesson_print.py
@@ -0,0 +1,138 @@
+# 🐍 PYTHON ASOSLARI — 1-DARS
+# 📌 Mavzu: Python bilan tanishuv
+# 1️⃣ Python nima?
+# Python — bu:
+# yuqori darajali (o‘qish oson)
+# kuchli
+# ommabop dasturlash tili
+# Python qayerda ishlatiladi?
+# 🌐 Web (Django, Flask)
+# 🤖 Sun’iy intellekt (AI, ML)
+# 📊 Data analysis
+# 🎮 O‘yinlar
+# 🤖 Telegram botlar
+# 🖥 Avtomatlashtirish
+# 👉 Boshlovchilar uchun ENG YAXSHI til.
+
+# Mavzu: print(), comments, escape characters
+
+# Birinchi dastur - print() pythonda ekranga chiqarish uchun ishlatiladi
+# 📌print chiqarish - degani
+print ("Salom, Python!")
+
+
+# Qoidalar:
+# Matn " " yoki ' ' ichida yoziladi lekin bir xil uslubni tanlagan yaxshi
+# Katta-kichik harf muhim (Print ❌, print ✅)
+
+
+print("Salom")
+print("Men Python o‘rganayapman")
+print("Bugun 1-dars")
+# ⛔ Python har qatorni alohida chiqaradi.
+
+
+# Izohlar (Comments #)
+# Izoh — kodga tushuntirish, Python o‘qimaydi.
+# Bu mening birinchi Python dasturim
+print("Hello world")
+
+
+print("Mening ismim Abdullatif")
+print("Men Python o'rganishni boshladim")
+print("Bugun 1-dars")
+print("Python oson va qiziqarli")
+
+
+# Bir nechta narsani bir qatorda chiqarish
+print("Mening ismim", "Abdullatif")
+print("Yoshim", 20)
+
+
+# Sonlarni chiqarish
+print(10)
+print(5+3)
+print(10*2)
+# Sonlarni " " va ' ' ushbu belgilarsiz yoziladi
+
+
+# sep - ajratuvchi belgi
+print("Python", "Java", "C++", sep= " | ")
+# Default sep=" " (bo'sh joy)
+
+
+# end - qator oxiri
+print("Salom", end=" ")
+print("Dunyo")
+
+
+# Maxsus belgilar (\n, \t)
+print("Salom\nPython")
+print("Ism:\tAbdullatif")
+# \n - new line degani yangi qatordan chiqaradi
+# \t - horizontal tab so'zlar orasidan bo'sh joy tashlaydi probel emas
+# odatda u 4-8ta probelga teng bo'ladi (terminalga bog'liq)
+
+
+# 🔥 Escape characters (escape sequences)
+# Ya’ni: \ bilan boshlanadigan maxsus ma’noga ega belgilar
+# Eng ko'p ishlatiladigan belgilar
+# \n - new line yangi qator
+# \t - horizontal tab tab (4ta)
+# \\ - backslash \ chiqaradi
+# \' - single quote ' chiqaradi biror so'zni bir tirnoqqa olish kerak bo'lganda
+# \" - double quote " qo'sh tirnoq chiqaradi biror so'z yoki gapni
+# kod ichida qo'sh tirnoqqa olmoqchi bo'lsak kodga ta'siri bo'lmaydi
+print("U \"Python\" dedi") 
+
+
+x = 10 # Bunda x ning qiymati 10ga teng python x ni 10 deb o'qiydi
+print("x ning qiymati:", x)
+# Dastur nima qilayotganini tushunish uchun juda muhin
+
+
+# Ko'rinishni chiroyli qilish
+print("************")
+print("** PYTHON **")
+print("************")
+
+
+# 🔥 ENG KATTA XATO (YODDA TUT)
+print("Yoshim", 20) # bu ko'rinish to'g'ri
+print("Yoshim " + 20) # bu ko'rinish xato
+# Sababi matn + son qo'shilmaydi (hozircha)
+
+
+# 🧩 1-Vazifa (oson) Quyidagilarni chiqaring:
+# Ism: Abdullatif
+#        Familiya: Axmedov
+#        Yosh: 20
+#        Kasb: Dasturchi
+
+
+# 🧩 2-Vazifa (o'rtacha) Quyidagilarni chiqaring:
+# sep va end ishlatib chiqaring:
+# bularni hammasini 1 qatorda chiqsin
+#        Python | Java | C++
+
+
+# 🧩 3-Vazifa (qiyinroq) Quyidagini chiqaring:
+# Quyidagi shaklni chiqaring:
+#          *****
+#          *   *
+#          *   *
+#          *****
+
+
+# 🧩 4-Vazifa (qiyin) Quyidagini chiqaring:
+# Faqat print() yordamida:
+# 1ta print() dan foydalanib
+#          Men Python o‘rganayapman
+#          Bugun 1-dars
+#          Bu juda qiziqarli!
+
+
+# 🧩 5-Vazifa (fikrlash):
+# Natija nima bo'ladi?
+print("A", "B", "C", sep="-", end="!")
+print("Python")
